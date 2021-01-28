889f8f253794afad5fb3c8d33f98ff74cb05ea0f
Nathan <s3717205@student.rmit.edu.au>
2021-01-28 15:24:08 +1100

    Cleaned up routing

--- website.py
+++ website.py
@@ -16,13 +16,43 @@ class LoginPage(webapp2.RequestHandler):
         self.response.out.write(template.render('./templates/login.html', template_values))
 
     def post(self):
+        # TODO user account check
         username = self.request.get('username')
         password = self.request.get('password')
         self.response.out.write("<h1>Your username: " + username + "  Your password: " + password + "</h1>")
 
+class SignupPage(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'placeholder': "PLACEHOLDER"
+        }
+        self.response.out.write(template.render('./templates/signup.html', template_values))
+
+    #def post(self):
+        # todo write to datastore
+        #check password length and requirements
+        #input full name, address, email, phone, password,
+
+class AdminPage(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'placeholder': "PLACEHOLDER"
+        }
+        self.response.out.write(template.render('./templates/admin.html', template_values))
+
+class HomePage(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'placeholder': "PLACEHOLDER"
+        }
+        self.response.out.write(template.render('./templates/home.html', template_values))
+
 app = webapp2.WSGIApplication([
-    webapp2.Route(r'/', handler=IndexPage, name='home'),
+    webapp2.Route(r'/', handler=IndexPage, name='index'),
     webapp2.Route(r'/login', handler=LoginPage, name='login'),
+    webapp2.Route(r'/signup', handler=SignupPage, name='signup'),
+    webapp2.Route(r'/admin', handler=AdminPage, name='admin'),
+    webapp2.Route(r'/home', handler=HomePage, name='home'),
     ], debug=True)
 
 def main():
